f1abae9add5a5d626e33185a7c115f200940ad34
linyicheng <yicheng080212@gmail.com>
2024-04-28 15:34:50 +0800

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,4 +1,4 @@
 <?php
-$dsn = "mysql:host=localhost;charset=utf8;dbname=db15";
+$dsn = "mysql:host=localhost;charset=utf8;dbname=53NationalJunior";
 $pdo = new PDO($dsn, 'root', '');
 session_start();
